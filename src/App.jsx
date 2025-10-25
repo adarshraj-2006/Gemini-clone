8d5065cf011c1673982e5bbb39800b2d2a439b54
adarshraj-2006 <adarsh.raj.parashar2006@gmail.com>
2025-10-25 23:32:45 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Main } from './components/main/Main'
+import Side_bar from './components/side_bar/side_bar'
+export const App = () => {
+  return(
+    <>
+    <Side_bar />
+    <Main />
+    </>
+    
+     
+    
+  
+  
+  )
+}
+
+export default App
